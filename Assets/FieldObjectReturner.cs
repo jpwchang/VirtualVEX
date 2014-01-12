e8e67ef0953e8c8003dfd6eb362fd1cc9d6ac53e
Jonathan Chang <jonathan.chang13@gmail.com>
2014-01-12 10:04:54 -0800

    Added scripts to return fallen objects to field

---
+++ Assets/FieldObjectReturner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Returns fallen objects to the field
+/// </summary>
+public class FieldObjectReturner : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        ReturnableObject obj = other.attachedRigidbody.gameObject.GetComponent<ReturnableObject>();
+        if(obj != null)
+        {
+            //Restore the object to its last known on-field position
+            other.transform.position = obj.getPosition;
+        }
+    }
+}
---
+++ Assets/GameObjects2014/ReturnableObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents any object that can be returned to the field upon leaving
+/// </summary>
+public class ReturnableObject : MonoBehaviour {
+
+    private Vector3 position_; //Track the object's position
+
+    public Vector3 getPosition
+    {
+        get { return position_; }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Don't update the position if it is outside the field
+        if (Mathf.Abs(this.transform.position.x) < 1.6 && Mathf.Abs(this.transform.position.z) < 1.6)
+            position_ = this.transform.position;
+	}
+}
